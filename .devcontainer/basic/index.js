d10fab5f1370d5e54b773dcc5b7c72f169ca6921
Deepak Yadav <dipakyadav1564@gmail.com>
2026-07-21 12:32:44 +0000

    type conversion

---
+++ .devcontainer/basic/index.js
@@ -0,0 +1,12 @@
+
+let score = "33abc";
+console.log(typeof score );
+let valueIn= Number(score);
+console.log(typeof valueIn);
+console.log(valueIn);
+//"33abc"=> NaN (Not a Number)
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+// "deepak" => NaN
