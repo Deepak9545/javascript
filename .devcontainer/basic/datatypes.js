26fe8260cbb253d415ee300d41f27a2c954f7cf4
Deepak Yadav <dipakyadav1564@gmail.com>
2026-07-21 17:28:03 +0000

    datatypes summary

---
+++ .devcontainer/basic/datatypes.js
@@ -0,0 +1,11 @@
+
+// primitive data types=> value ka copy banta hai
+// 7 types of primitive data types in JavaScript
+// string, number, boolean, null, undefined, symbol, bigint
+// non-primitive data types=> value ka reference banta hai
+// non-primitive data types in JavaScript=> object, array, function
+const arr = [1, 2, 3, 4, 5];
+const obj = { name: "deepak", age: 22, city: "delhi" };
+const func = function () {
+  console.log("hello");
+};
