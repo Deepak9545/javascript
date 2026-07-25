bc317dfba3496d3bda6cf89995eb7e7d4a428ed4
Deepak9545 <dipakyadav1564@gmail.com>
2026-07-25 17:12:22 +0530

    array part1

---
+++ .devcontainer/basic/array.js
@@ -0,0 +1,34 @@
+
+// array
+const array = [1,2,3,4,5]
+console.log(array)
+// shallow copy me same referrence point hota 
+// deep copy me alag referrence point hota hai
+//const myarr1 = new Array(1,2,3,4)
+//console.log(myarr1[2]);
+
+// array methods
+//array.push(7) // add element at last
+//console.log(array)
+// array.pop() // remove last element
+
+// array.shift() // remove first element
+// array.unshift(0) // add element at first
+// console.log(array)
+// join method converted array into string
+//const myarr2 = array.join(" ")
+//console.log(myarr2)
+//console.log( typeof myarr2) 
+
+
+// slice method +++++++
+const myarr3 = array.slice(1,3)
+console.log(myarr3)
+ 
+// splice method +++++++
+const myarr4 = array.splice(1,3)
+console.log(myarr4)
+console.log(array)
+
+
+
