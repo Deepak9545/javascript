bd49b4d11c0fd445b2208a51e6f51bf37ed6e619
Deepak9545 <dipakyadav1564@gmail.com>
2026-07-25 23:45:59 +0530

    object part 1

---
+++ .devcontainer/basic/object.js
@@ -0,0 +1,32 @@
+
+// object literal
+
+const Mysym = Symbol("key1");
+const person ={
+    name: "deepak",
+    [Mysym]: "mykey1",
+    age:20,
+    add: "ballia",
+    "full name": "deepak kumar"
+}
+// console.log(person["full name"]); // access property with big bracket because we cant access dot property
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.add);
+// console.log(person["add"]);
+
+// person.add = "delhi" // update property
+// Object.freeze(person) // freeze object property
+// person.add = "mumbai" // update property
+// console.log(person); 
+
+
+person.greeting = function(){
+    console.log("hello");
+}
+person.greetingTwo = function(){
+    console.log(`hello deepak ${this.age} ${this.add}`); // this keyword is used to access the property of object
+}
+
+console.log(person.greeting()); // access method
+console.log(person.greetingTwo()); // access method
