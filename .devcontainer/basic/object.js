50daaea396024aae1e83000d1bb81005198795cf
Deepak9545 <dipakyadav1564@gmail.com>
2026-07-26 18:58:08 +0530

    object part 2

--- .devcontainer/basic/object.js
+++ .devcontainer/basic/object.js
@@ -1,33 +1,54 @@
 
-// object literal
+const obj = new Object(); //  create a singleton object
+//const obj = {} // create a singleton object using object literal syntax that means non singleton object
+obj.name = "deepak"
+obj.id ="abac123"
+obj.email = "ashs@example.com"
+//console.log(obj); 
 
-const Mysym = Symbol("key1");
-const person ={
-    name: "deepak",
-    [Mysym]: "mykey1",
-    age:20,
-    add: "ballia",
-    "full name": "deepak kumar",
-    email:"deepak@example.com"
+const regularuser = {
+    email: "regularuser@example.com",
+    fullname: {
+        firstname: "deepak",
+        lastname: "kumar"
+    }
 }
-// console.log(person["full name"]); // access property with big bracket because we cant access dot property
-// console.log(person.name);
-// console.log(person.age);
-// console.log(person.add);
-// console.log(person["add"]);
+//console.log(regularuser.fullname.firstname) // access nested object property
+const obj1 = {1: "one", 2: "two", 3: "three"}
+const obj2 = {7: "seven", 8: "eight", 9: "nine"}
 
-// person.add = "delhi" // update property
-// Object.freeze(person) // freeze object property
-// person.add = "mumbai" // update property
-// console.log(person); 
+const obj3 = {...obj1, ...obj2} // merge two objects with spread operator
+//console.log(obj3)
 
 
-person.greeting = function(){
-    console.log("hello");
+const user = [
+    {
+        id: 1,
+        name: "deepak",
+        email: "deepak@example.com"
+    }, 
+    {
+        id: 2,
+        name: "john",
+        email: "john@example.com"
+    }
+]
+user[1].name
+// console.log(obj)
+
+// console.log(Object.keys(obj)) // return array of keys // important hai
+// console.log(Object.values(obj)) // return array of values // important hai
+// console.log(Object.entries(obj)) // return array of key value pairs // important hai
+
+const course = {
+    name: "javascript",
+    price: 999,
+    courseInstructor: "deepak",
+    courseDuration: "3 months"
 }
-person.greetingTwo = function(){
-    console.log(`hello deepak ${this.age} ${this.add} ${this.email}`); // this keyword is used to access the property of object
-}
-
-console.log(person.greeting()); // access method
-console.log(person.greetingTwo()); // access method
+// console.log(course.courseInstructor) // access object property
+// course.courseInstructor = "john" // update object property
+ //console.log(course.courseInstructor) // access updated object property
+const {price: yadav} = course // object destructuring and renaming the property name
+//console.log(price)
+console.log(yadav) // access object property using destructuring
