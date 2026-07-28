9764056dc8a8b1e70b1374aab6731bc47aff92aa
Deepak9545 <dipakyadav1564@gmail.com>
2026-07-28 19:55:40 +0530

    function part2

--- .devcontainer/basic/basic2/function.js
+++ .devcontainer/basic/basic2/function.js
@@ -18,4 +18,35 @@ function loginusermessage(username) {
 }
 
 // console.log(loginusermessage("deepak"))
-console.log(loginusermessage()) // undefined k sath print hoga kyuki humne username pass nhi kiya
+// console.log(loginusermessage()) // undefined k sath print hoga kyuki humne username pass nhi kiya
+
+function calculate( val1,val2,...nums1) // rest operator
+{ 
+    return nums1
+}
+//console.log(calculate(200,300,400,500))
+
+const user = {
+    Username: "deepak",
+    price: 200
+}
+function handleobj(anyobject){
+    console.log(`username is ${anyobject.Username} and price is ${anyobject.price}`)
+
+}
+//handleobj(user)
+// direct object bhi pass lr sakte hai array bhi
+handleobj({
+    Username: "ad",
+
+    price: 500
+})
+
+const array = [200,300,400,500]
+ 
+function returnvalue(getarray){
+    return getarray[1]
+}
+console.log(array)
+console.log(returnvalue(array))
+console.log(returnvalue[200,300,400])
